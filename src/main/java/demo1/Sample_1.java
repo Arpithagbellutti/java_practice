57a79a570a50c0fd6ef2e017e7dfee8712fa9242
DESKTOP-V4LR938\Dell <arpithagbellutti@gmail.com>
2024-09-30 08:32:31 +0530

    Modified demo files

---
+++ src/main/java/demo1/Sample_1.java
@@ -0,0 +1,14 @@
+package demo1;
+
+public class Sample_1 {
+//     protected static int x = 10;
+     public static int y = 50;
+//     private int z = 20;
+}
+//class A extends Sample_1{
+//    public static void main(String[] args) {
+//        System.out.println(Sample_1.x);
+//    }
+//
+//
+//        }
---
+++ src/test/java/demo_2/Sample_2.java
@@ -0,0 +1,9 @@
+package demo_2;
+
+import demo1.Sample_1;
+
+public class Sample_2 {
+    public static void main(String[] args) {
+        System.out.println(Sample_1.y);
+    }
+}
